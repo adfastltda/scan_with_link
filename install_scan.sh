35f2122602ce659bbb0a1d43f70683440cfa8eb7
adfastltda <143732424+adfastltda@users.noreply.github.com>
2023-08-31 14:56:40 -0300

    Create install_scan.sh

---
+++ install_scan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt update -y
+apt upgrade -y
+pkg install python -y
+pip install requests
+pkg install git
+
+# Clonar o repositório do GitHub
+git clone https://github.com/adfastltda/scan_with_link.git
+
+# Navegar para o diretório do repositório
+cd scan_with_link
+
+# Executar o script scan.py
+python scan.py
